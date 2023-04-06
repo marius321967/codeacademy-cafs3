c35b584ee71e2a20a41c7ce4dec39f65b8e262b3
marius321967 <marius.dras@gmail.com>
2023-04-06 21:01:29 +0300

    tasks -> user relationship

--- src/app/Models/Task.php
+++ src/app/Models/Task.php
@@ -2,12 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'deadline', 'is_completed'];
+
+    protected $casts = [
+        'is_completed' => 'boolean',
+    ];
+
+    public function scopeCompleted(Builder $query): void
+    {
+        $query->where('is_completed', true);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
