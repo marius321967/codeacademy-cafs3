307a645dba64a3a87f76ad620dea85abfa356fd8
marius321967 <marius.dras@gmail.com>
2023-04-11 20:41:21 +0300

    exchange rate service no longer uses auth key

--- src/app/Services/ExchangeRates/ExampleApiExchangeRateService.php
+++ src/app/Services/ExchangeRates/ExampleApiExchangeRateService.php
@@ -8,16 +8,12 @@ use Illuminate\Support\Facades\Http;
 class ExampleApiExchangeRateService implements ExchangeRateServiceInterface
 {
 
-  public function __construct(private string $authKey)
-  {
-  }
-
   /**
    * @return ExchangeRate[]
    */
   public function getExchangeRates(): array
   {
-    $response = Http::get('https://api.exchangerate.host/latest', ['auth_key' => $this->authKey]);
+    $response = Http::get('https://api.exchangerate.host/latest');
 
     $ratesUnprocessed = $response->json()['rates'];
     $result = [];
