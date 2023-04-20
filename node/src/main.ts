560b67790d037a611dfa875e239816ba81788730
marius321967 <marius.dras@gmail.com>
2023-04-20 21:22:07 +0300

    register datepicker

--- node/src/main.ts
+++ node/src/main.ts
@@ -1,13 +1,16 @@
-import { createApp } from 'vue'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 import { createPinia } from 'pinia'
-import TaskPriorityIndicator from './components/TaskPriorityIndicator.vue'
-
+import { createApp } from 'vue'
 import App from './App.vue'
+import TaskPriorityIndicator from './components/TaskPriorityIndicator.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.component('TaskPriorityIndicator', TaskPriorityIndicator)
+app.component('DatePicker', VueDatePicker)
+
 app.use(createPinia())
 app.use(router)
 
