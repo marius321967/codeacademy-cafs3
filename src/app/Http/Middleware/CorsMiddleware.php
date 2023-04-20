99064257a80f75ad687a2c0733aea7181531f84e
marius321967 <marius.dras@gmail.com>
2023-04-20 20:18:38 +0300

    add another cors header

--- src/app/Http/Middleware/CorsMiddleware.php
+++ src/app/Http/Middleware/CorsMiddleware.php
@@ -16,6 +16,7 @@ class CorsMiddleware
   public function handle($request, Closure $next)
   {
     return $next($request)
-      ->header('Access-Control-Allow-Origin', '*');
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Headers', 'content-type');
   }
 }
