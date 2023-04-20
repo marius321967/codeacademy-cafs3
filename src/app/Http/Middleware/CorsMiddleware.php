b202b3176430031b52fd3b2141408eeeb9222c62
marius321967 <marius.dras@gmail.com>
2023-04-20 22:01:49 +0300

    Add methods to cors

--- src/app/Http/Middleware/CorsMiddleware.php
+++ src/app/Http/Middleware/CorsMiddleware.php
@@ -17,6 +17,7 @@ class CorsMiddleware
   {
     return $next($request)
       ->header('Access-Control-Allow-Origin', '*')
-      ->header('Access-Control-Allow-Headers', 'content-type');
+      ->header('Access-Control-Allow-Headers', 'content-type')
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   }
 }
