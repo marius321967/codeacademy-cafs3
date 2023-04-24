8fb287de4b63de4f40eb646af6fa59afc45196d4
marius321967 <marius.dras@gmail.com>
2023-04-24 20:28:54 +0300

    allow authorization header in cors

--- src/app/Http/Middleware/CorsMiddleware.php
+++ src/app/Http/Middleware/CorsMiddleware.php
@@ -17,7 +17,7 @@ class CorsMiddleware
   {
     return $next($request)
       ->header('Access-Control-Allow-Origin', '*')
-      ->header('Access-Control-Allow-Headers', 'content-type')
+      ->header('Access-Control-Allow-Headers', 'content-type, authorization')
       ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   }
 }
