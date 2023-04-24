b34f01c04ebf892c4e6ce6ceacbc5142e5d057d0
marius321967 <marius.dras@gmail.com>
2023-04-24 20:30:33 +0300

    show users list in admin

--- node/src/views/Admin/Users.vue
+++ node/src/views/Admin/Users.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import type { IUser } from '../../interfaces'
+import { useAuthStore } from '../../stores/auth'
+
+const users = ref<IUser[]>([])
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.httpClient?.get('/users').then((res) => (users.value = res.data))
+})
+</script>
+
 <template>
-  <div>Here are all your users</div>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="user in users">
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.role }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
