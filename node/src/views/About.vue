5fd67b0d2d3186b8de2e09dc12ea923745373055
marius321967 <marius.dras@gmail.com>
2023-04-25 20:21:12 +0300

    reformat about page

--- node/src/views/About.vue
+++ node/src/views/About.vue
@@ -4,11 +4,14 @@
   <div class="container">
     <h1 class="mt-5">About us</h1>
     <p class="lead">
-      Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.
+      Pin a footer to the bottom of the viewport in desktop browsers with this
+      custom HTML and CSS.
     </p>
     <p>
       Use
-      <a href="/docs/5.0/examples/sticky-footer-navbar/">the sticky footer with a fixed navbar</a>
+      <a href="/docs/5.0/examples/sticky-footer-navbar/"
+        >the sticky footer with a fixed navbar</a
+      >
       if need be, too.
     </p>
   </div>
