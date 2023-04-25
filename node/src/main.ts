979685b6f7224beef7a17489adb5c8a4e9b31165
marius321967 <marius.dras@gmail.com>
2023-04-25 20:18:18 +0300

    slot samples

--- node/src/main.ts
+++ node/src/main.ts
@@ -5,11 +5,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import TaskPriorityIndicator from './components/TaskPriorityIndicator.vue'
 import router from './router'
+import Card from './components/ui/Card.vue'
+import SubmitButton from './components/ui/SubmitButton.vue'
 
 const app = createApp(App)
 
 app.component('TaskPriorityIndicator', TaskPriorityIndicator)
 app.component('DatePicker', VueDatePicker)
+app.component('SubmitButton', SubmitButton)
+app.component('Card', Card)
 
 app.use(createPinia())
 app.use(router)
--- node/src/views/Tasks/TaskForm.vue
+++ node/src/views/Tasks/TaskForm.vue
@@ -76,67 +76,62 @@ watch(
 </script>
 
 <template>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">
-        {{ isEditing ? 'Edit task' : 'New task' }}
-      </h5>
+  <card>
+    <template #title>
+      {{ isEditing ? 'Edit task' : 'New task' }}
+    </template>
 
-      <div class="mb-3">
-        <label for="title" class="form-label">Task</label>
-        <input
-          class="form-control"
-          type="text"
-          id="title"
-          v-model.trim="form.title"
-        />
-        <div class="form-text text-danger" v-if="errors.title">
-          {{ errors.title.join(' ') }}
-        </div>
-      </div>
-
-      <div class="mb-3">
-        <label for="deadline" class="form-label">Deadline</label>
-        <date-picker
-          v-model="form.deadline"
-          format="yyyy-MM-dd HH:mm:ss"
-        ></date-picker>
-        <div class="form-text text-danger" v-if="errors.deadline">
-          {{ errors.deadline.join(' ') }}
-        </div>
-      </div>
-
-      <task-priority-input v-model="form.priority"></task-priority-input>
-
-      <div class="form-check mb-3">
-        <input
-          class="form-check-input"
-          id="is_completed"
-          type="checkbox"
-          v-model="form.is_completed"
-        />
-        <label class="form-check-label" for="is_completed">Completed</label>
-        <div class="form-text text-danger" v-if="errors.is_completed">
-          {{ errors.is_completed.join(' ') }}
-        </div>
-      </div>
-
-      <div class="mb-3">
-        <button
-          class="btn btn-outline-success"
-          @click="submit"
-          :disabled="isBusy"
-        >
-          <div class="spinner-border spinner-border-sm" v-if="isBusy"></div>
-          Create
-        </button>
-      </div>
-
-      <div class="mb-3" v-if="errorMessage">
-        <small class="text-danger">{{ errorMessage }}</small>
+    <div class="mb-3">
+      <label for="title" class="form-label">Task</label>
+      <input
+        class="form-control"
+        type="text"
+        id="title"
+        v-model.trim="form.title"
+        ref="titleInputElement"
+      />
+      <div class="form-text text-danger" v-if="errors.title">
+        {{ errors.title.join(' ') }}
       </div>
     </div>
-  </div>
+
+    <div class="mb-3">
+      <label for="deadline" class="form-label">Deadline</label>
+      <date-picker
+        v-model="form.deadline"
+        format="yyyy-MM-dd HH:mm:ss"
+      ></date-picker>
+      <div class="form-text text-danger" v-if="errors.deadline">
+        {{ errors.deadline.join(' ') }}
+      </div>
+    </div>
+
+    <task-priority-input v-model="form.priority"></task-priority-input>
+
+    <div class="form-check mb-3">
+      <input
+        class="form-check-input"
+        id="is_completed"
+        type="checkbox"
+        v-model="form.is_completed"
+      />
+      <label class="form-check-label" for="is_completed">Completed</label>
+      <div class="form-text text-danger" v-if="errors.is_completed">
+        {{ errors.is_completed.join(' ') }}
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <submit-button :disabled="isBusy" @click="submit">
+        <div class="spinner-border spinner-border-sm" v-if="isBusy"></div>
+        Create
+      </submit-button>
+    </div>
+
+    <div class="mb-3" v-if="errorMessage">
+      <small class="text-danger">{{ errorMessage }}</small>
+    </div>
+  </card>
 </template>
 
 <style>
